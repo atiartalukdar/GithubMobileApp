21fb172db8a36ccc646a2ceeb3d058d5cb9abc28
Atiar Talukdar <atiartalukdar@gmail.com>
2024-04-29 04:49:10 +0900

    fix: code cleanup

--- app/src/main/java/info/atiar/githubmobileapp/features/users/domain/model/User.kt
+++ app/src/main/java/info/atiar/githubmobileapp/features/users/domain/model/User.kt
@@ -4,15 +4,4 @@ data class User(
     val login: String = "",
     val id: Int = 0,
     val avatar_url: String = ""
-) {
-    fun doesMatchSearchQuery(query: String): Boolean {
-        val matchingCombinations = listOf(
-            "$login",
-            "$id",
-        )
-
-        return matchingCombinations.any {
-            it.contains(query, ignoreCase = true)
-        }
-    }
-}
+)
