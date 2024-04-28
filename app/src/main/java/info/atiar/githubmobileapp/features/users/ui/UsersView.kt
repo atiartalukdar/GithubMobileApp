20925c70d3023176f07a960959333a1bf99681ea
Atiar Talukdar <atiartalukdar@gmail.com>
2024-04-29 04:47:46 +0900

    update search design

--- app/src/main/java/info/atiar/githubmobileapp/features/users/ui/UsersView.kt
+++ app/src/main/java/info/atiar/githubmobileapp/features/users/ui/UsersView.kt
@@ -3,6 +3,7 @@ package info.atiar.githubmobileapp.features.users.ui
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -57,6 +62,7 @@ fun UsersContent(
     searchQuery: (String) -> Unit
 ) {
     var query by remember { mutableStateOf("") }
+    var isFocused by remember { mutableStateOf(false) }
 
     LoadingDialog(isShowingDialog = state.isLoading)
     Scaffold(
@@ -72,7 +78,7 @@ fun UsersContent(
                 verticalAlignment = Alignment.Top,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0xFF4182AA))
+                    .background(MaterialTheme.colorScheme.primary)
                     .padding(8.dp)
 
             ) {
@@ -85,9 +91,18 @@ fun UsersContent(
                     label = { Text("Search") },
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 24.dp)
+                        .padding(top = 24.dp),
+                    trailingIcon = {
+                        if (query.isNotEmpty()) {
+                            RightCrossIcon {
+                                query = ""
+                                searchQuery(query)
+                            }
+                        } else {
+                            SearchIcon()
+                        }
+                    },
                 )
-
             }
 
             LazyColumn(
@@ -131,4 +146,25 @@ fun UsersContent(
 
         }
     }
+}
+
+@Composable
+fun RightCrossIcon(onIconClick: () -> Unit) {
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "Clear Text",
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { onIconClick() }
+    )
+}
+
+@Composable
+fun SearchIcon() {
+    Icon(
+        imageVector = Icons.Default.Search,
+        contentDescription = "Search Text",
+        modifier = Modifier
+            .padding(8.dp)
+    )
 }
