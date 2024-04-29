de2e5393d277de17ef8438ce0d49856f2b0e5983
Atiar Talukdar <atiartalukdar@gmail.com>
2024-04-29 20:19:05 +0900

    feat: webview added

--- app/src/main/java/info/atiar/githubmobileapp/features/user_profile/ui/UserProfileView.kt
+++ app/src/main/java/info/atiar/githubmobileapp/features/user_profile/ui/UserProfileView.kt
@@ -2,6 +2,8 @@ package info.atiar.githubmobileapp.features.user_profile.ui
 
 import android.annotation.SuppressLint
 import android.util.Log
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,11 +17,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import info.atiar.githubmobileapp.features.user_profile.domain.model.UserProfile
@@ -41,6 +47,8 @@ internal fun UserProfileView(
 fun UserProfileContent(
     state: UserProfileViewState
 ) {
+    var url by remember { mutableStateOf("") }
+    var shouldOpenWebview by remember { mutableStateOf(false) }
     LoadingDialog(isShowingDialog = state.isLoading)
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -61,16 +69,41 @@ fun UserProfileContent(
             LazyColumn(
                 modifier = Modifier.padding(horizontal = 16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
-
             ) {
                 items(state.userRepos) {
-                    RepoItemView(userRepo = it){repoUrl ->
+                    RepoItemView(userRepo = it) { repoUrl ->
                         Log.e("Git Repo Link:", repoUrl)
+                        url = repoUrl
+                        shouldOpenWebview = true
                     }
                 }
             }
         }
     }
+
+    if (shouldOpenWebview) {
+        InAppWebView(url = url) {
+            if (it) {
+                shouldOpenWebview = false
+            }
+        }
+    }
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun InAppWebView(url: String, onReleaseWebview: (Boolean) -> Unit) {
+    AndroidView(
+        factory = ::WebView,
+        update = { webView ->
+            webView.webChromeClient = WebChromeClient()
+            webView.settings.javaScriptEnabled = true
+            webView.loadUrl(url)
+        },
+        onRelease = {
+            onReleaseWebview(true)
+        }
+    )
 }
 
 @Preview
